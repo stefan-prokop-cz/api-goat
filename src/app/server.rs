834de2670b593cb7b6b3e4859b49506dda8f5835
Stefan Prokop <sprokop1010@seznam.cz>
2020-08-19 20:24:41 +0200

    🔊 Add logger

--- src/app/server.rs
+++ src/app/server.rs
@@ -1,14 +1,21 @@
 use super::config::Config;
 use super::router;
+use actix_web::middleware::Logger;
 use actix_web::{dev, App, HttpServer};
+use env_logger;
 
 pub fn start() -> dev::Server {
     Config::load();
     let config = Config::get();
-    HttpServer::new(|| App::new().configure(router::new))
+    env_logger::init();
+    info!("Running with {:?}", config);
+    HttpServer::new(|| App::new().configure(router::new).wrap(Logger::default()))
         .bind(format!("127.0.0.1:{}", config.server_port))
         .unwrap_or_else(|error| {
-            panic!("Could not bind server to address 127.0.0.1:3000: {}", error)
+            panic!(
+                "Could not bind server to address 127.0.0.1:{}: {}",
+                config.server_port, error
+            )
         })
         .run()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate log;
+
 mod app;
 
 use app::server;
