9b112398d24e2e53fa28d81e8cd13b325a197e6a
Stefan Prokop <sprokop1010@seznam.cz>
2020-08-24 19:42:38 +0200

    ✨ Use postgres instead of mysql

--- src/app/database/mod.rs
+++ src/app/database/mod.rs
@@ -1,14 +1,14 @@
 use crate::app::config;
-use diesel::mysql::MysqlConnection;
+use diesel::pg::PgConnection;
 use diesel::Connection;
 
 pub mod schema;
 pub mod user;
 
-pub fn connect() -> MysqlConnection {
+pub fn connect() -> PgConnection {
     let config = config::Config::get();
-    let connection = MysqlConnection::establish(&config.database_url)
-        .unwrap_or_else(|error| panic!("Cannot connect to MySQL: {}", error));
-    info!("Successfully connected to MySQL");
+    let connection = PgConnection::establish(&config.database_url)
+        .unwrap_or_else(|error| panic!("Cannot connect to Postgres: {}", error));
+    info!("Successfully connected to Postgres");
     connection
 }
